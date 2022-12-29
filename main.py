c823b15e616d27fa4278efaa192fbd4525bfe4f3
Svetla Alexandrova <121426643+Svetla-Alexandrova@users.noreply.github.com>
2022-12-29 14:30:13 +0000

    Search file by name in computer

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 from pathlib import Path
-import zipfile
 
 root_dir = Path(".")
-destination_path = Path('destination')
+searched_item = "15"
 
-for path in root_dir.glob("*.zip"):
-    with zipfile.ZipFile(path, 'r') as zf:
-        final_path = destination_path / Path(path.stem)
-        zf.extractall(path=final_path)
+for path in root_dir.rglob('*'):
+  if path.is_file():
+    if searched_item in path.stem:
+      print(path.absolute())
+
+
+
+
+
+
+
+# from pathlib import Path
+# import zipfile
+
+# root_dir = Path(".")
+# destination_path = Path('destination')
+
+# for path in root_dir.glob("*.zip"):
+#     with zipfile.ZipFile(path, 'r') as zf:
+#         final_path = destination_path / Path(path.stem)
+#         zf.extractall(path=final_path)
+        
